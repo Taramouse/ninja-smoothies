e198055b049e59ec267f66ff8ab041d195475220
taramouse <nickykeyse@gmail.com>
2019-07-06 20:09:53 +0100

    Added home component.

---
+++ src/components/Home.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="home container">
+    <div class="card" v-for="smoothie in smoothies" :key="smoothie.id">
+      <div class="card-content">
+        <h2 class="indigo-text">{{ smoothie.title }}</h2>
+        <ul class="ingredients">
+          <li v-for="(ing, index) in smoothie.ingredients" :key="index">
+            <span class="chip">{{ ing }}</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+      smoothies: [
+        { title: 'Ninja Brew', slug: 'ninja-brew', ingredients: ['banana', 'rasberry', 'milk'], id: '1' },
+        { title: 'Morning Mood', slug: 'morning-mood', ingredients: ['mango', 'lime', 'lemon', 'juice'], id: '2' },
+        { title: 'Nickys Nightmare', slug: 'nickys-nightmare', ingredients: ['coffee', 'peanutbutter', 'milk'], id: '3' }
+      ]
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+.home {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-gap: 50px;
+  margin-top: 60px;
+}
+.home h2 {
+  font-size: 1.8em;
+  text-align: center;
+  margin-top: 0;
+}
+.home .ingredients {
+  margin: 30px auto;
+}
+.home .ingredients li {
+  display: inline-block;
+}
+</style>
