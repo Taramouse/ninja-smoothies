ee2c2dba6558aba5a382d2469926782a135b0592
taramouse <nickykeyse@gmail.com>
2019-07-08 16:23:21 +0100

    Added icon to delete smoothies.

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,10 +2,11 @@
   <div class="home container">
     <div class="card" v-for="smoothie in smoothies" :key="smoothie.id">
       <div class="card-content">
+        <i class="material-icons delete" @click="deleteSmoothie(smoothie.id)">delete</i>
         <h2 class="indigo-text">{{ smoothie.title }}</h2>
         <ul class="ingredients">
           <li v-for="(ing, index) in smoothie.ingredients" :key="index">
-            <span class="chip">{{ ing }}</span>
+            <span class="chip indigo lighten-3">{{ ing }}</span>
           </li>
         </ul>
       </div>
@@ -23,6 +24,14 @@ export default {
         { title: 'Morning Mood', slug: 'morning-mood', ingredients: ['mango', 'lime', 'lemon', 'juice'], id: '2' },
         { title: 'Nickys Nightmare', slug: 'nickys-nightmare', ingredients: ['coffee', 'peanutbutter', 'milk'], id: '3' }
       ]
+    }
+  },
+  methods: {
+    deleteSmoothie (id) {
+      this.smoothies = this.smoothies.filter(smoothie => {
+        // return only the smoothies to the array which do not match the smoothie to delete
+        return smoothie.id != id
+      })
     }
   }
 }
@@ -46,5 +55,13 @@ export default {
 }
 .home .ingredients li {
   display: inline-block;
+}
+.home .delete {
+  position: absolute;
+  top: 4px;
+  right: 4px;
+  cursor: pointer;
+  color: #90a4ae;
+  font-size: 1.4em;
 }
 </style>
