6ecb2c9701355b0a734b4401a566e33dff91eb90
taramouse <nickykeyse@gmail.com>
2019-07-08 19:38:40 +0100

    Added links for Home and AddSmoothie components.

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,10 +2,10 @@
   <div class="navbar">
     <nav class="nav-extended indigo darken-2">
       <div class="nav-content">
-        <router-link to="">
+        <router-link :to="{ name: 'Home' }">
           <span class="nav-title">Ninja Smoothies</span>
           <a href="" class="btn-floating btn-large halfway-fab pink">
-            <router-link to="">
+            <router-link :to="{ name: 'AddSmoothie' }">
               <i class="material-icons">add</i>
             </router-link>
           </a>
