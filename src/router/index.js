ddddd7cb364042cac71e2372ead6e2efd552f374
taramouse <nickykeyse@gmail.com>
2019-07-08 19:37:41 +0100

    Added route for AddSmoothie component.

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from '@/components/Home'
+import AddSmoothie from '@/components/AddSmoothie'
 
 Vue.use(Router)
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'Home',
       component: Home
+    },
+    {
+      path: '/add-smoothie',
+      name: 'AddSmoothie',
+      component: AddSmoothie
     }
   ]
 })
